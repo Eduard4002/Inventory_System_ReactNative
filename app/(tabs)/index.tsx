6ea5d0705d8aab0ce908fae8454d9252f6b37bb8
Eduard4002 <mihiceduard@gmail.com>
2025-03-28 21:26:06 +0100

    Added .env to gitignore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { ScrollView, Text, View, Image, FlatList } from "react-native";
 import ItemCard from "@/components/ItemCard";
 export default function Index() {
+  
   return (
     <View className="flex-1 bg-primary">
       <ScrollView
@@ -8,12 +9,6 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
-        <ItemCard title="Brasno" id={0} />
-        <ItemCard title="Brasno" id={0} />
-        <ItemCard title="Brasno" id={0} />
-        <ItemCard title="Brasno" id={0} />
-        <ItemCard title="Brasno" id={0} />
-        <ItemCard title="Brasno" id={0} />
       </ScrollView>
     </View>
   );
